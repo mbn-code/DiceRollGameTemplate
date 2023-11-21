d7e7db628aee0093e98de88d5a776fb037b543ec
1cy50ng3b0b <66748817+CollinEdward@users.noreply.github.com>
2023-11-21 14:10:59 +0100

    Update dice_roll_game.py

--- dice_roll_game.py
+++ dice_roll_game.py
@@ -1,39 +1,63 @@
 import time
 import random
-# Roll the dice and see what you get, then wait for the ai to roll a dice and see who wins
 
-print("""
-Welcome to dice roll. You are playing against indi AI.
-""") # Here you can change the sleep int higher or lower for wait time before the game starts
-time.sleep(2)
+def roll_dice():
+    return random.randint(1, 6)
 
+def print_dice_roll(player, value):
+    print(f"\n{player} rolled a {value}")
 
-while 1:
-    Roll_dice = input("Please roll the dice (roll/exit): ") 
+def print_round_result(player, player_value, ai_value, result):
+    print(f"\n{player} rolled {player_value} | AI rolled {ai_value}")
+    time.sleep(1)
+    print(result)
 
-    if Roll_dice.lower() == "roll":
-        print("\n\n\nrolling dice. . .")
-        time.sleep(random.randint(0,3))
-        player_dice_roll = random.randint(1,6)
-        print("\nYou rolled a " + str(player_dice_roll))
+def print_score(player_wins, ai_wins):
+    print(f"\nCurrent Score - You: {player_wins} | AI: {ai_wins}")
 
-        print("\nNow indi AI is rolling. . .")
-        time.sleep(random.randint(0,4))
-        
-        ai_dice_roll = random.randint(1,6)
-        if ai_dice_roll > player_dice_roll:
-            print("\nThe AI rolled " + str(ai_dice_roll) + " which is higher then your roll which was " + str(player_dice_roll))
-            time.sleep(2)
-            print("\nYou lost. . .\n\n")
-        elif ai_dice_roll < player_dice_roll:
-            print("The AI rolled " + str(ai_dice_roll) + " Which is lower then your roll which was " + str(player_dice_roll))
-            time.sleep(1)
-            print("You won!!\n\n\n")
+def main():
+    print("Welcome to Dice Roll. You are playing against indi AI.")
+    time.sleep(2)
 
-    # ask for exit or keep playing
-    elif Roll_dice.lower() == "exit":
-        exit()
-    elif Roll_dice != "roll":
-        print("please enter either 'roll' or 'exit'")
-    elif Roll_dice != "exit":
-        print("please enter either 'roll' or 'exit'")
+    player_wins = 0
+    ai_wins = 0
+    rounds = 1
+
+    while True:
+        print(f"\nRound {rounds}")
+        player_input = input("Please roll the dice (roll/exit): ")
+
+        if player_input.lower() == "roll":
+            print("\nRolling dice...")
+            time.sleep(random.randint(1, 3))
+
+            player_dice = roll_dice()
+            print_dice_roll("You", player_dice)
+
+            print("\nNow indi AI is rolling...")
+            time.sleep(random.randint(1, 4))
+
+            ai_dice = roll_dice()
+            print_dice_roll("AI", ai_dice)
+
+            if ai_dice > player_dice:
+                print_round_result("You", player_dice, ai_dice, "You lost.\n")
+                ai_wins += 1
+            elif ai_dice < player_dice:
+                print_round_result("You", player_dice, ai_dice, "You won!\n")
+                player_wins += 1
+            else:
+                print("\nIt's a tie!\n")
+
+            print_score(player_wins, ai_wins)
+            rounds += 1
+
+        elif player_input.lower() == "exit":
+            print("\nThanks for playing!")
+            exit()
+
+        else:
+            print("Please enter either 'roll' or 'exit'")
+
+if __name__ == "__main__":
+    main()
